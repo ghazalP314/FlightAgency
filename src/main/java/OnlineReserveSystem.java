405e6ed53734638b6f5324e240de43f125ca2691
dadddddy <mohammadbabamahmoudi@gmail.com>
2017-02-12 16:01:30 +0330

    Project desin reformed (public ArrayList<Flight> ask_about_flight(String origin,String dest,String date,String comp_server_addr,int comp_port) throws IOException{Server and OnlineReserveSystem classes )

--- src/main/java/OnlineReserveSystem.java
+++ src/main/java/OnlineReserveSystem.java
@@ -1,16 +1,23 @@
+
+
 import common.Flight;
 import common.Reserve;
 import companies.ConnectFlightCompany;
 import user.Person;
 
-import java.io.IOException;
+
+import java.net.*;
+import java.io.*;
 import java.util.ArrayList;
 
+
 /**
- * Created by ghazal on 2/10/2017 AD.
+ * Created by daddy on 2/11/17.
  */
 public class OnlineReserveSystem {
-    //public OnlineReserveSystem(){}
+    public Socket socket;
+    public PrintWriter writer;
+    public BufferedReader reader;
     public ArrayList<Flight> ask_about_flight(String origin,String dest,String date,String comp_server_addr,int comp_port) throws IOException{
         ArrayList<Flight> result=new ArrayList<Flight>();
         ConnectFlightCompany c=new ConnectFlightCompany(comp_server_addr,comp_port);
@@ -34,12 +41,63 @@ public class OnlineReserveSystem {
         c.close();
         return result;
     }
-    public void reserve(Flight f,char _class,ArrayList<Person> p,String comp_server,int comp_port)throws IOException{
+    public void reserve(Flight f, char _class, ArrayList<Person> p, String comp_server, int comp_port)throws IOException{
         Reserve r=new Reserve(f,_class,p);
         ConnectFlightCompany c=new ConnectFlightCompany(comp_server,comp_port);
         String[] result=c.QueryReserve(r).split(" ");
         r.set_token(result[0]);
     }
+
+    public void close()throws IOException{
+        socket.close();
+        writer.close();
+        reader.close();
+
+    }
+
+    public OnlineReserveSystem(int portNumber , String IP, int port) throws  IOException{
+        ServerSocket listener = new ServerSocket(portNumber);
+        socket = listener.accept();
+        writer = new PrintWriter(socket.getOutputStream(), true);
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer.println("********************************************************");
+        writer.println("*** Welcome to the online ticket reservation service ***");
+        writer.println("********************************************************");
+        userRequest(IP,port);
+
+//        System.out.println(reader.readLine());
+
+    }
+    public  void userRequest(String IP,int port )throws IOException{
+        writer.println("What's your request?");
+        String in = reader.readLine();
+        String[] in_splitted = in.split(" ");
+
+        if(in_splitted[0].equals("search")){
+            ArrayList<Flight> flights= ask_about_flight(in_splitted[1], in_splitted[2], in_splitted[3],IP,port);
+            for (int i =0;i<flights.size();i++){
+                writer.println("Flight : "+flights.get(i).getAirline()+" "+flights.get(i).getFlight_num()+" Departure : "
+                        +flights.get(i).getT_departure().charAt(0)+flights.get(i).getT_departure().charAt(1)+":"+flights.get(i).getT_departure().charAt(2)+flights.get(i).getT_departure().charAt(3)
+                        +" Arrival : "+flights.get(i).getT_arrival().charAt(0)+flights.get(i).getT_arrival().charAt(1)+":"+
+                        flights.get(i).getT_arrival().charAt(2)+flights.get(i).getT_arrival().charAt(3)
+                        +" Airplane : "+flights.get(i).getAirplane());
+            }
+
+
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        OnlineReserveSystem ourServer = new OnlineReserveSystem(9092,"188.166.78.119",8081);
+        ourServer.close();
+
+
+    }
+
 
 
 }
